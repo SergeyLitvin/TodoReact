93b5c47b1237741c3fad575683174983eabb9772
Front-end Dev <front.dev@cryptobitup.net>
2019-10-01 22:30:37 +0300

    Create component TodoListItem

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import TodoListItem from './todo-list-item';
+
 const TodoList = () => {
     return (
         <ul>
-            <li>Learn React</li>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
             <li>Build awesome Todo react app</li>
             <li>Drink green tea</li>
         </ul>
