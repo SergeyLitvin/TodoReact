73755ad872489b747f695cc99a61e27009cccdbd
front.dev <front.dev@cryptobitup.net>
2019-10-03 18:00:12 +0300

    Add styles

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = ({todos}) => {
+const TodoList = ({
+    todos
+}) => {
 
-    const elements = todos.map((item) =>{
-        return (
-            <li key={item.id}>
-                <TodoListItem { ...item} />
-            </li>
+    const elements = todos.map((item) => {
+        return (<li key={
+            item.id
+        } >
+            <TodoListItem {...item} />  </li >
         );
     });
-    return (
-        <ul>
-            { elements }
-        </ul>
-    )
+    return (<ul className="list-group" > {
+        elements
+    } </ul>)
 };
 
-export  default TodoList;
+export default TodoList;
