b35224a0df121f404dd6cbd4feba719d80eb893b
front.dev <front.dev@cryptobitup.net>
2019-10-09 17:59:26 +0300

    Refactor structure project. Divided components into folders.

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppHeader from "../app-header/";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import './app.css';
+
+
+const App = () => {
+
+    const todoData = [
+        {id: 1, label: 'Drink green tea.', important: false},
+        {id: 2, label: 'Make awesome todo-react app', important: true},
+        {id: 3, label: 'Take a walk on nature.', important: false}
+    ];
+
+return (
+    <div className="container">
+        <AppHeader/>
+        <div className="search-panel-wrap">
+            <SearchPanel/>
+            <ItemStatusFilter/>
+        </div>
+
+        <TodoList todos={todoData}/>
+    </div>
+)};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './index.css';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ItemStatusFilter from './components/item-status-filter';
-import TodoList from './components/todo-list';
-
-const App = () => {
-
-    const todoData = [
-        {id: 1, label: 'Drink green tea.', important: false },
-        {id: 2, label: 'Make awesome todo-react app', important: true},
-        {id: 3, label: 'Take a walk on nature.', important: false}
-    ];
-
-    return (
-        <div className="container">
-            <AppHeader />
-            <div className="search-panel-wrap">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-
-            <TodoList todos={todoData} />
-        </div>
-    )
-};
+import App from "./components/app";
 
 ReactDom.render( <App /> , document.getElementById('root'));
