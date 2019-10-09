176944ccf79a457b5c107f4ead80d1f7401ea128
front.dev <front.dev@cryptobitup.net>
2019-10-09 16:54:17 +0300

    Filed compile in todo-list.js

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -8,13 +8,13 @@ const TodoList = ({ todos }) => {
     const elements = todos.map((item) => {
 
         // console.log(item);
-        //
-        // let { id, ...itemProps } = item;
-        // console.log(id);
-        // console.log(itemProps);
+
+        let { id, ...itemProps } = item;
+        console.log(id);
+        console.log(itemProps);
         return (
-            <li key={item.id} className="list-group-item">
-                <TodoListItem { ...item } />
+            <li key={id} className="list-group-item">
+                <TodoListItem { ...itemProps } />
             </li>
         );
     });
