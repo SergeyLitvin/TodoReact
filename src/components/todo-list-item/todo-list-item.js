13fbce1513e58e57ff9835cdaa5483d7d91f037b
Front-end Dev <front.dev@cryptobitup.net>
2019-10-12 20:51:10 +0300

    Change react component func to react component class from todo-list-item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important = false}) => {
-    const style = {
-        color: important ? 'tomato' : 'black'
-    };
+export default class TodoListItem extends Component {
 
-    return (
-        <div className="todo-list-item">
+
+    render() {
+
+        const {label, important = false} = this.props;
+
+            const style = {
+                color: important ? 'tomato' : 'black'
+            };
+
+            return (
+                <div className="todo-list-item">
             <span
                 className="todo-list-item-label flex-grow-1"
                 style={style}>
                 {label}
             </span>
-            <div className="btn-wrap">
-                    <button type="button"
-                            className="btn btn-outline-danger btn-sm">
-                        <i className="fa fa-trash"></i>
-                    </button>
-                    <button type="button"
-                            className="btn btn-outline-success btn-sm">
-                        <i className="fa fa-exclamation"></i>
-                    </button>
-            </div>
-        </div>
-    );
-};
-
-export default TodoListItem;
+                    <div className="btn-wrap">
+                        <button type="button"
+                                className="btn btn-outline-danger btn-sm">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                        <button type="button"
+                                className="btn btn-outline-success btn-sm">
+                            <i className="fa fa-exclamation"></i>
+                        </button>
+                    </div>
+                </div>
+            );
+    }
+}
