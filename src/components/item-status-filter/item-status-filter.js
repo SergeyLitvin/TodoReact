2ff3253c493f3f13ad4852ef6cf08001eb991c33
Front-end Dev <front.dev@cryptobitup.net>
2019-10-12 21:01:25 +0300

    Change react component func to react component class from item-status-filter

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <div className="btn btn-info">All</div>
-            <div className="btn btn-outline-secondary">Active</div>
-            <div className="btn btn-outline-secondary">Done</div>
-        </div>
-    )
-};
+export default class ItemStatusFilter extends Component  {
 
-export default ItemStatusFilter;
+    render() {
+        return (
+            <div className="btn-group">
+                <div className="btn btn-info">All</div>
+                <div className="btn btn-outline-secondary">Active</div>
+                <div className="btn btn-outline-secondary">Done</div>
+            </div>
+        )
+    }
+
+}
