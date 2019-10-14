17acd579717eb30e96fd427fceedeb7e525ef0b3
front.dev <front.dev@cryptobitup.net>
2019-10-14 16:58:37 +0300

    Function delete item on click btn trash

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import AppHeader from "../app-header/";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
@@ -8,27 +7,44 @@ import ItemStatusFilter from "../item-status-filter";
 import './app.css';
 
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        {id: 1, label: 'Drink green tea.', important: false},
-        {id: 2, label: 'Make awesome todo-react app', important: true},
-        {id: 3, label: 'Take a walk on nature.', important: false}
-    ];
+    state = {
+        todoData: [
+            {id: 1, label: 'Drink green tea.', important: false},
+            {id: 2, label: 'Make awesome todo-react app', important: true},
+            {id: 3, label: 'Take a walk on nature.', important: false}
+        ]
+    };
 
-return (
-    <div className="container">
-        <AppHeader/>
-        <div className="search-panel-wrap">
-            <SearchPanel/>
-            <ItemStatusFilter/>
-        </div>
+    deleteItem = (id) => {
+        this.setState( ({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            // todoData.splice(idx, 1); // нельзя изменять существующий массив или объект (bad practice react)
 
-        <TodoList
-            todos={todoData}
-            onDeleted={ (id) => console.log('Delet ', id)}
-        />
-    </div>
-)};
+            const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
 
-export default App;
+            return {
+                todoData: newArray
+            }
+        });
+    };
+
+    render() {
+        return (
+            <div className="container">
+                <AppHeader/>
+                <div className="search-panel-wrap">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList
+                    todos={ this.state.todoData }
+                    onDeleted={ this.deleteItem }
+                />
+            </div>
+        )
+    }
+
+};
